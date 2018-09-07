18f5939d84e3d65ee75d1885b0176e24526a643e
Ramon Larivoir <rlarivoir@hotmail.com>
2018-09-07 11:08:26 -0300

    Ajustando layout e instanciando os botoes com Click Listener no MainActivity

--- app/src/main/java/br/ufjf/dcc196/exercicio01/MainActivity.java
+++ app/src/main/java/br/ufjf/dcc196/exercicio01/MainActivity.java
@@ -1,14 +1,17 @@
 package br.ufjf.dcc196.exercicio01;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String ALUNO_QTD = "0";
-    public static final String SERVIDOR_QTD = "0";
-    public static final String EXTERNO_QTD = "0";
+    public static final String PESSOA_NOME = "nome";
+    public static final int ALUNO_ID = 1;
+    public static final int SERVIDOR_ID = 2;
+    public static final int EXTERNO_ID = 3;
 
     private Button btnAluno;
     private Button btnServidor;
@@ -20,7 +23,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnAluno = (Button) findViewById(R.id.btn_aluno);
-        btnExterno = (Button) findViewById(R.id.btn_externo);
+        btnAluno.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentAluno = new Intent(MainActivity.this, AlunoActivity.class);
+                startActivityForResult(intentAluno, MainActivity.ALUNO_ID);
+            }
+        });
+
         btnServidor = (Button) findViewById(R.id.btn_servidor);
+        btnServidor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentServidor = new Intent(MainActivity.this, ServidorActivity.class);
+                startActivityForResult(intentServidor, MainActivity.SERVIDOR_ID);
+            }
+        });
+
+        btnExterno = (Button) findViewById(R.id.btn_externo);
+        btnExterno.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentExterno = new Intent(MainActivity.this, ExternoActivity.class);
+                startActivityForResult(intentExterno, MainActivity.EXTERNO_ID);
+            }
+        });
     }
 }
